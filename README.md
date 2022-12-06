0452e41d24e159dffae8239f7fb9cbe383313490
stefanoyoyo <31388199+stefanoyoyo@users.noreply.github.com>
2022-12-06 10:21:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular---primeng-come-selezionare-e-fare-drag-and-drop-or-copy-di-una-riga
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeng-tablereorder-dragdrop-demo-jge21k)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "./productservice";
+import { Product } from "./product";
+import { differenceBy } from "lodash";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html"
+})
+export class AppComponent {
+  availableProducts: Product[] = [];
+  draggedProducts: Product[] = [];
+  selectedProducts: Product[] = [];
+
+  cols: any[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService
+      .getProductsSmall()
+      .then(data => (this.availableProducts = data));
+
+    this.cols = [{ field: "name", header: "Name" }];
+  }
+
+  drop() {
+    this.selectedProducts = [...this.selectedProducts, ...this.draggedProducts];
+    // this.availableProducts = differenceBy(
+    //   this.availableProducts,
+    //   this.draggedProducts,
+    //   "code"
+    // );
+  }
+
+  dragStart(data) {
+    console.log("drag startt");
+  }
+  dragEnd() {
+    console.log("drag end");
+
+    this.drop();
+    this.draggedProducts = [];
+  }
+}
